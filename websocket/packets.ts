5f34b56693ce5e338a279ac535b03b37c10df94b
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-12-24 11:54:28 -0800

    update packet format

--- websocket/packets.ts
+++ websocket/packets.ts
@@ -19,34 +19,73 @@ export interface ctsPacketData {
 }
 
 export enum stcPacketIds {
-	update = 1 | 0b1000000,
+	addEntities = 1 | 0b1000000,
+	updateTransforms = 2 | 0b1000000,
+	updateFishLevels = 3 | 0b1000000,
+	updateHealth = 4 | 0b1000000,
+	updateXp = 5 | 0b1000000,
+	updateFoods = 6 | 0b1000000,
 }
 export interface stcPacketData {
-	[stcPacketIds.update]: {
-		animals: {
-			updateLength: number;
-			updateList: {
-				id: number;
-				name: string;
-				health: number;
-				fishLevel: number;
-				x: number;
-				y: number;
-				rotation: number;
-			}[];
-			removeLength: number;
-			removeList: number[];
-		};
-		foods: {
-			addLength: number;
-			addList: {
-				id: number;
-				type: number;
-				x: number;
-				y: number;
-			}[];
-			removeLength: number;
-			removeList: number[];
-		};
+	[stcPacketIds.addEntities]: {
+		length: number;
+		list: {
+			id: number;
+			fishLevel: number;
+			name: string;
+			health: number;
+			x: number;
+			y: number;
+			rotation: number;
+		}[];
+	};
+	[stcPacketIds.updateTransforms]: {
+		updateLength: number;
+		updateList: {
+			id: number;
+			x: number;
+			y: number;
+			rotation: number;
+		}[];
+		removeLength: number;
+		removeList: number[];
+	};
+	[stcPacketIds.updateFishLevels]: {
+		updateLength: number;
+		updateList: {
+			id: number;
+			fishLevel: number;
+		}[];
+	};
+	[stcPacketIds.updateHealth]: {
+		updateLength: number;
+		updateList: {
+			id: number;
+			count: number;
+			hpChanges: number[];
+		}[];
+	};
+	[stcPacketIds.updateXp]: {
+		updateLength: number;
+		updateList: {
+			id: number;
+			xp: number;
+		}[];
+	};
+	[stcPacketIds.updateFoods]: {
+		addLength: number;
+		addList: {
+			id: number;
+			x: number;
+			y: number;
+		}[];
+		updateLength: number;
+		updateList: {
+			id: number;
+			x: number;
+			y: number;
+		}[];
+		removeLength: number;
+		removeList: number[];
 	};
 }
