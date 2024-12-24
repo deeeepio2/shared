e6bdf883ff5635b1f279961cd41b358e00d2698b
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-12-24 11:27:36 -0800

    compression/decompression script

---
+++ compression/index.ts
@@ -0,0 +1,27 @@
+/**
+ * https://gist.github.com/Explosion-Scratch/357c2eebd8254f8ea5548b0e6ac7a61b?permalink_comment_id=4598581#gistcomment-4598581
+ */
+
+export const compress = async (
+	str: string,
+	encoding = "gzip" as CompressionFormat,
+): Promise<ArrayBuffer> => {
+	const byteArray = new TextEncoder().encode(str);
+	const cs = new CompressionStream(encoding);
+	const writer = cs.writable.getWriter();
+	writer.write(byteArray);
+	writer.close();
+	return new Response(cs.readable).arrayBuffer();
+};
+
+export const decompress = async (
+	byteArray: string[],
+	encoding = "gzip" as CompressionFormat,
+): Promise<string> => {
+	const cs = new DecompressionStream(encoding);
+	const writer = cs.writable.getWriter();
+	writer.write(byteArray);
+	writer.close();
+	const arrayBuffer = await new Response(cs.readable).arrayBuffer();
+	return new TextDecoder().decode(arrayBuffer);
+};
